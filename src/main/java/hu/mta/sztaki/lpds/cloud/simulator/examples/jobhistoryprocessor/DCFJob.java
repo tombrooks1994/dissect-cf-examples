83a84e41133f164262768b578f98f94dbb46c56a
Gabor Kecskemeti (mb) <kecskemeti.gabor@sztaki.mta.hu>
2015-07-15 23:10:26 +0200

    DCFJob to follow the updated DistSysJavaHelpers api changes

--- src/main/java/hu/mta/sztaki/lpds/cloud/simulator/examples/jobhistoryprocessor/DCFJob.java
+++ src/main/java/hu/mta/sztaki/lpds/cloud/simulator/examples/jobhistoryprocessor/DCFJob.java
@@ -39,15 +39,6 @@ public class DCFJob extends Job {
 	/**
 	 * Forwards the constructor to the Job's appropriate constructor
 	 * 
-	 * @param jobLine
-	 */
-	public DCFJob(String jobLine) {
-		super(jobLine);
-	}
-
-	/**
-	 * Forwards the constructor to the Job's appropriate constructor
-	 * 
 	 * @param submit
 	 * @param queue
 	 * @param exec
@@ -55,9 +46,11 @@ public class DCFJob extends Job {
 	 * @param user
 	 * @param executable
 	 */
-	public DCFJob(long submit, long queue, long exec, int nprocs, String user,
-			String executable) {
-		super(submit, queue, exec, nprocs, user, executable);
+	public DCFJob(String id, long submit, long queue, long exec, int nprocs,
+			double ppCpu, long ppMem, String user, String group,
+			String executable, Job preceding, long delayAfter) {
+		super(id, submit, queue, exec, nprocs, ppCpu, ppMem, user, group,
+				executable, preceding, delayAfter);
 	}
 
 	/**
